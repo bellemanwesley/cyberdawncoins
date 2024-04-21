a95c6984148b11e5689cb7aac9a19557a89dd543
Wesley Belleman <bellemanwesley@gmail.com>
2024-04-21 15:16:14 -0400

    fixing submission

--- coins_site/coins_site/settings.py
+++ coins_site/coins_site/settings.py
@@ -26,7 +26,7 @@ with open(BASE_DIR / '../../keys/django') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coins.cyberdawnexercise.com','localhost']
 
 
 # Application definition
