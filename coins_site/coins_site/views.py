74eaccaacaed69b23a43ea0e6948a19d07c29267
Wesley Belleman <bellemanwesley@gmail.com>
2024-04-21 15:18:58 -0400

    fixing submission

--- coins_site/coins_site/views.py
+++ coins_site/coins_site/views.py
@@ -15,7 +15,7 @@ def form(request):
     elif request.method == 'POST':
         #Get the value of the input field
         input_value = request.POST.get('access_code')
-        with open(BASE_DIR / '../../keys/access_code') as f:
+        with open(BASE_DIR / '../../keys/secret') as f:
             access_code = f.read().strip()
         #if access code is equal access_code, then render the form.html
         if input_value == access_code:
