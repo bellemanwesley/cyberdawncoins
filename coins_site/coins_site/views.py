9b27c3f7d90807563cd43d3e3122d9a60b379fcd
Wesley Belleman <bellemanwesley@gmail.com>
2024-04-21 15:09:03 -0400

    updating user interface

--- coins_site/coins_site/views.py
+++ coins_site/coins_site/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 def home(request):
-	return(render(request,'home.html',{}))
+	return(render(request,'home.html',{"incorrect_hidden": "true"}))
+
+def form(request):
+    #If request type is get, then redirect to home
+    if request.method == 'GET':
+        return render(request,'home.html',{"incorrect_hidden": "true"})
+    elif request.method == 'POST':
+        #Get the value of the input field
+        input_value = request.POST.get('access_code')
+        with open(BASE_DIR / '../../keys/access_code') as f:
+            access_code = f.read().strip()
+        #if access code is equal access_code, then render the form.html
+        if input_value == access_code:
+            return(render(request,'form.html',{"incorrect_hidden": "false"}))
